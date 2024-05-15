001b089fdb3559b71996d656e1276909d5357b74
Alexbi <alexa_gomez@euneiz.com>
2024-05-15 10:08:54 +0200

    intentar solucionar muchos errores

--- recuperacion1505/Program.cs
+++ recuperacion1505/Program.cs
@@ -4,10 +4,13 @@ List<string> NAMES = new List<string> ();
 //CREAR LISTA DE PUNTOS
 List<int> POINT = new List<int>();
 
+int points = -1,
+
 //CREAR LAS LISTAS BASE (BAQUEIRA BERET/FIERBRUNN/YETI EXTREME)
 
+
 //BAQUEIRA
-List<string> BAQUEIRA = new List<string>()
+List<string> BAQUEIRA = ; new List<string>()
 {
     "Jonathan Penfield",
     "Liam Rivera",
@@ -50,9 +53,54 @@ List<int> FIERBRUNNPOINT = new List<int>();
 }
 
 
-object value = BAQUEIRA + BAQUEIRAPOINT.ForEach(Console.WriteLine);
+//yeti
+List<string> YETI = new List<string>()
+{
+    "Anna Orlova",
+    "Ludovic Guillo",
+    "Katie Anderson",
+    "Jonathan Penfield",
+    "Liam Rivera​",
+    "Estelle Rizzolio",
+   
+  
+};
+List<int> YETIpoints = new List<int>();
+{
+    // Añadir elementos a la lista
+    YETIpoints.Add(500);
+    YETIpoints.Add(400);
+    YETIpoints.Add(300);
+    YETIpoints.Add(150);
+    YETIpoints.Add(100);
+    YETIpoints.Add(50);
+}
 
 
+
+
+{ for (int x = 0; x < BAQUEIRAPOINT.Count; x++) ;
+
+    if (BAQUEIRAPOINT[x] > points)
+     {
+        BAQUEIRAPOINT = points;
+
+        points = BAQUEIRAPOINT[x];
+    }
+
+    NAMES = points,
+
+NAMES = BAQUEIRAPOINT;
+
+}
+
+
+// imprimir en pantalla el vehiculo con mas kms son listVehicleName con maxkms
+Console.WriteLine(NAMES, points);
+
+
+BAQUEIRAPOINT.ForEach(Console.WriteLine);
 
 //RECORRER CADA UNA DE LAS LISTAS Y COMPROVAR EL VALOR DE CADA PERSONA Y ORDENAR DE MAYOR A MENOR
+
 //IMPRIMIR POR PANTALLA LA LISTA DE NOMBRES + LISTA DE PUNTOS
