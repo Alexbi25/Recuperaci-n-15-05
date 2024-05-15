2f1f9ae9f8ddf45c65c3a865bfa2ceb4936c4b73
Alexbi <alexa_gomez@euneiz.com>
2024-05-15 09:02:05 +0200

    Añadido parte de algoritmo

--- recuperacion1505/Program.cs
+++ recuperacion1505/Program.cs
@@ -1,5 +1,35 @@
 ﻿//CREAR LISTA DE NOMBRES
+
+
 //CREAR LISTA DE PUNTOS
+
+
+
 //CREAR LAS LISTAS BASE (BAQUEIRA BERET/FIERBRUNN/YETI EXTREME)
+    //BAQUEIRA
+List<string> BAQUEIRA = new List<string>()
+{
+    "Jonathan Penfield",
+    "Liam Rivera",
+    "Katie Anderson",
+    "Anna Orlova",
+    "Ludovic Guillot​",
+    "​Estelle Rizzolio",
+};
+List<int> BAQUEIRAPUNTOS = new List<int> { 500, 400, 300, 150, 100, 50 };
+
+//FIERBRUNN
+List<string> FIERBRUNN = new List<string>()
+{
+    "Jonathan Penfield",
+    "Liam Rivera",
+    "Katie Anderson",
+    "Anna Orlova",
+    "Ludovic Guillot​",
+    "​Estelle Rizzolio",
+};
+List<int> FIERBRUNNPUNTOS = new List<int> { 500, 400, 300, 150, 100, 50 };
+
+
 //RECORRER CADA UNA DE LAS LISTAS Y COMPROVAR EL VALOR DE CADA PERSONA Y ORDENAR DE MAYOR A MENOR
 //IMPRIMIR POR PANTALLA LA LISTA DE NOMBRES + LISTA DE PUNTOS
